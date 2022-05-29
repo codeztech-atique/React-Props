c2c96e1de924b9f0421f5cbb8506f0b871a59ead
codeztech-atique <atique1224@gmail.com>
2022-05-29 21:50:50 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -2,10 +2,15 @@ import * as React from 'react';
 import './style.css';
 
 export default function App(props) {
+  const shoot = () => {
+    alert('Great Shot!');
+  };
+
   return (
     <div>
       <h1>Hello {props.name}!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>Start click on the button to see the magic :)</p>
+      <button onClick={shoot}>Take the Shot!</button>
     </div>
   );
 }
